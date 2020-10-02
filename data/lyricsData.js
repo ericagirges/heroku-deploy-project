8a9a3aaeac93db0f84fcc916b319f35faf1ba1fe
Erica Girges <ericafiorenza@gmail.com>
2020-10-01 19:26:20 -0700

    added more data

--- data/lyricsData.js
+++ data/lyricsData.js
@@ -4,6 +4,11 @@ let lyrics = [
     verse: `His palms are sweaty, knees weak, arms are heavy. There's vomit on his sweater already, mom's spaghetti`,
     artist: "Eminem",
   },
+  {
+      id: 2,
+      verse: "I had a marvelous time ruining everything.",
+      artist: "Taylor Swift",
+  }
 ];
 
 module.exports = lyrics;
