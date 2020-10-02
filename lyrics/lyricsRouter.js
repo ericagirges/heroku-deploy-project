1d502617a66bfd1d87a068e837c3b27f609a506d
Erica Girges <ericafiorenza@gmail.com>
2020-10-01 20:03:11 -0700

    GET endpoint by id

--- lyrics/lyricsRouter.js
+++ lyrics/lyricsRouter.js
@@ -12,4 +12,23 @@ router.get("/", (req, res) => {
   }
 });
 
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+
+  try {
+    // retrieve the lyrics from the "database" - try / catch will catch this if there's a failure
+    const found = Lyrics.find((lyrics) => lyrics.id === id);
+
+    if (found) {
+      res.status(200).json({ data: found });
+      return;
+    }
+    res.status(404).json({ message: "Could not find the request lyrics." });
+  } catch (e) {
+    res
+      .status(500)
+      .json({ errorMessage: "The lyrics could not be retrieved." });
+  }
+});
+
 module.exports = router;
